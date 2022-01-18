2e2583c5cff165e1494441a11903f2722aa14fb1
Alok Singh <alokbeniwal@gmail.com>
2022-01-18 00:08:36 -0800

    add step fn

--- src/utils.py
+++ src/utils.py
@@ -5,9 +5,10 @@ from __future__ import annotations
 
 from math import floor
 from typing import Dict, Literal, Mapping, Optional
-
+from pathlib import Path
 import torch
 from torch import Tensor
+import numpy as np
 
 Stage = Literal["train", "val", "test"]
 
@@ -32,3 +33,25 @@ def split(n: int, splits: Optional[Mapping[Stage, float]] = None) -> Dict[Stage,
 def rmse(pred: Tensor, target: Tensor):
     """Root mean squared error."""
     return torch.nn.functional.mse_loss(pred, target).sqrt()
+
+def step_at_n(n: float = 3.5, max: float = 12):
+    """Returns tuple of x and y which corresponds to a function that outputs 0 at index < n, and 1 at index > n.
+
+    n: wavelength to step down at
+    max: max wavelength
+    
+    
+    """
+    
+    wavelength = torch.load(Path("wavelength.pt"))
+    low = 0
+    high = 0
+    for i, wl in enumerate(wavelength[0]):
+        if wl < n:
+            low = i
+        if wl < max:
+            high = i
+
+    x = wavelength[0][low:high]
+    y = [1 for i in range(low)] + [0 for i in range(high-low)]
+    return (x, y)
