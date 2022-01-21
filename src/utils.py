526f935b39c2a5745a8be55c4e56ea2fa6937f4c
Alok Singh <alokbeniwal@gmail.com>
2022-01-21 15:06:53 -0800

    update step constructor

--- src/utils.py
+++ src/utils.py
@@ -4,11 +4,12 @@
 from __future__ import annotations
 
 from math import floor
-from typing import Dict, Literal, Mapping, Optional
 from pathlib import Path
+from typing import Dict, Literal, Mapping, Optional
+
+import numpy as np
 import torch
 from torch import Tensor
-import numpy as np
 
 Stage = Literal["train", "val", "test"]
 
@@ -34,24 +35,12 @@ def rmse(pred: Tensor, target: Tensor):
     """Root mean squared error."""
     return torch.nn.functional.mse_loss(pred, target).sqrt()
 
-def step_at_n(n: float = 3.5, max: float = 12):
-    """Returns tuple of x and y which corresponds to a function that outputs 0 at index < n, and 1 at index > n.
 
-    n: wavelength to step down at
-    max: max wavelength
-    
-    
-    """
-    
-    wavelength = torch.load(Path("wavelength.pt"))
-    low = 0
-    high = 0
-    for i, wl in enumerate(wavelength[0]):
-        if wl < n:
-            low = i
-        if wl < max:
-            high = i
-
-    x = wavelength[0][low:high]
-    y = [1 for i in range(low)] + [0 for i in range(high-low)]
-    return (x, y)
+def step_tensor():
+    """Returns tensor of wavelengths, sorted high to low."""
+    # index at 0 because each row has the same info.
+    wavelens = torch.load(Path("wavelength.pt"))[0]
+    out = torch.zeros(len(wavelens), len(wavelens))
+    for r, row in enumerate(out):
+        out[r, : r + 1] = 1.0
+    return out
